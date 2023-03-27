0d92b8e3df8229237cbfbbb16e2fa44b1651144e
Kenneth <nitramnek@gmail.com>
2023-03-27 17:37:25 +0300

    Asignment Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+//K3n5c0d3
+// solution Starts Here!
+
+const employee = {
+        name: "Ken",
+        streetAddress: "80100 MBA"
+    }
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+        const newPersonel = { ...employee };
+        newPersonel[key] = value;
+        return newPersonel;
+    }
+//debugger
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+    }
+function deleteFromEmployeeByKey(employee, key) {
+        const newPersonel = { ...employee };
+        delete newPersonel[key];
+        return newPersonel;
+    }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+    }
+//debugger
